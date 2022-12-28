2d2aef1eabd44067db74c1e05f44bb2bbddb6170
niklasHagner <niklas.hagner@outlook.com>
2022-12-28 19:12:03 +0100

    Fresh default setup

---
+++ routes/about.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>Created by Hagge</p>
+      <p>This site uses the runtime "Deno" with the "Fresh" framework</p>
+    </main>
+  );
+}
