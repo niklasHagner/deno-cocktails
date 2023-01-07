eb2e7408f65a6f7abd7c7617230d6c1ad22974af
niklasHagner <niklas.hagner@outlook.com>
2023-01-07 20:22:12 +0100

    Move clickIngredientsFn

--- islands/CocktailCollection.tsx
+++ islands/CocktailCollection.tsx
@@ -17,57 +17,61 @@ export default function CocktailCollection(props: CocktailCollectionProps) {
   const [selectedIngredientNames, setSelectedIngredientNames] = useState([]);
   const allCocktails = props.cocktails;
 
+  function clickFilter(ingredientObj) {
+    if (selectedIngredientNames.includes(ingredientObj.name)) {
+      const indexOfItemToPop = selectedIngredientNames.indexOf(
+        ingredientObj.name,
+      );
+      selectedIngredientNames.splice(indexOfItemToPop, 1);
+    } else {
+      selectedIngredientNames.push(ingredientObj.name);
+    }
+    setSelectedIngredientNames(selectedIngredientNames);
+
+    let filteredCocktails = allCocktails.
+      filter((cocktail) => 
+      cocktail.ingredients
+      .some(x => selectedIngredientNames.includes(x.name)));
+      .sort((a, b) => {
+        const aMatchingIngredientCount = a.ingredients.filter(x => selectedIngredientNames.some(x.name)).length;
+        const bMatchingIngredientCount = b.ingredients.filter(x => selectedIngredientNames.some(x.name)).length;
+        const aPercentageOfTotal = aMatchingIngredientCount / a.ingredients.length;
+        const bPercentageOfTotal = bMatchingIngredientCount / b.ingredients.length;
+        // if (aPercentageOfTotal > 0.6 || bPercentageOfTotal > 0.6) {
+        //   return a < b ? -1 : 1;
+        // }  
+        // if (aMatchingIngredientCount < bMatchingIngredientCount) {
+        //   return -1;
+        // }
+        // if (aMatchingIngredientCount > bMatchingIngredientCount) {
+        //   return 1;
+        // }
+        return aPercentageOfTotal - bPercentageOfTotal;
+
+        return 0;
+      })
+      .reverse();
+
+    if (selectedIngredientNames.length <= 0) {
+      filteredCocktails = allCocktails;
+    }
+    setCocktails(filteredCocktails);
+    // console.log("onClick IngredientFilterButton in CocktailCollection");
+  }
+
   return (
     <>
       <h1>Pick ingredients to get cocktail suggestions</h1>
       <section class="ingredients">
-        {props.allIngredientObjectsFromFile.map((ingredientObj) => (
-          <IngredientFilterButton
-            ingredientObj={ingredientObj}
-            checked={false}
-            onClick={() => {
-              if (selectedIngredientNames.includes(ingredientObj.name)) {
-                const indexOfItemToPop = selectedIngredientNames.indexOf(
-                  ingredientObj.name,
-                );
-                selectedIngredientNames.splice(indexOfItemToPop, 1);
-              } else {
-                selectedIngredientNames.push(ingredientObj.name);
-              }
-              setSelectedIngredientNames(selectedIngredientNames);
-
-              let filteredCocktails = allCocktails.
-                filter((cocktail) => 
-                cocktail.ingredients
-                .some(x => selectedIngredientNames.includes(x.name)));
-                .sort((a, b) => {
-                  const aMatchingIngredientCount = a.ingredients.filter(x => selectedIngredientNames.some(x.name)).length;
-                  const bMatchingIngredientCount = b.ingredients.filter(x => selectedIngredientNames.some(x.name)).length;
-                  const aPercentageOfTotal = aMatchingIngredientCount / a.ingredients.length;
-                  const bPercentageOfTotal = bMatchingIngredientCount / b.ingredients.length;
-                  // if (aPercentageOfTotal > 0.6 || bPercentageOfTotal > 0.6) {
-                  //   return a < b ? -1 : 1;
-                  // }  
-                  // if (aMatchingIngredientCount < bMatchingIngredientCount) {
-                  //   return -1;
-                  // }
-                  // if (aMatchingIngredientCount > bMatchingIngredientCount) {
-                  //   return 1;
-                  // }
-                  return aPercentageOfTotal - bPercentageOfTotal;
-
-                  return 0;
-                })
-                .reverse();
-
-              if (selectedIngredientNames.length <= 0) {
-                filteredCocktails = allCocktails;
-              }
-              setCocktails(filteredCocktails);
-              // console.log("onClick IngredientFilterButton in CocktailCollection");
-            }}
-          />
-        ))}
+        {
+          props.allIngredientObjectsFromFile.map(ingredientObj => (
+            <IngredientFilterButton
+              ingredientObj={ingredientObj}
+              checked={false}
+              onClick={() => {clickFilter(ingredientObj);} }
+            />
+          ))
+        }
       </section>
 
       <h1>{cocktails.length} cocktails</h1>
