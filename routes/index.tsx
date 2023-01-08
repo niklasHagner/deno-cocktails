470fa16342248e19e2b7fc5810c2a108e5c5d097
niklasHagner <niklas.hagner@outlook.com>
2023-01-08 13:37:58 +0100

    Style using flex instead of grid

--- routes/index.tsx
+++ routes/index.tsx
@@ -43,6 +43,7 @@ export default function Page({ data }: PageProps<GodModel | null>) {
       </main>
       <footer>
         <p>Made by Hagge</p>
+        <p>using Deno + Fresh</p>
       </footer>
     </>
   );
