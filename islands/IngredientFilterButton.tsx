e67efa60be1967883a7ffe07e67825e700b3761e
niklasHagner <niklas.hagner@outlook.com>
2025-10-26 11:46:33 +0100

    Redesign filters as checkboxs

--- islands/IngredientFilterButton.tsx
+++ islands/IngredientFilterButton.tsx
@@ -11,14 +11,16 @@ interface IngredientFilterProps {
 export default function IngredientFilterButton(props: IngredientFilterProps) {
   const [checked, setChecked] = useState(props.checked);
   return (
-    <span>
-      <Button 
-        class="ingredient-btn" 
-        style={ checked === true ? "background-color: orange;" : "" }
-        data-is-booze={ props.ingredientObj.type === "booze" ? true: false }
-        onClick={() => { setChecked(!checked); props.onClick(); } }>
-        <span class="icon">{props.ingredientObj.name} {checked === true ? "🔴" : "⭕"}</span>
-      </Button>
-    </span>
+    <label class="ingredient-checkbox">
+      <input 
+        type="checkbox" 
+        id={`ingredient-${props.ingredientObj.name}`}
+        name={`ingredient-${props.ingredientObj.name}`}
+        checked={checked}
+        data-is-booze={props.ingredientObj.type === "booze" ? true : false}
+        onChange={() => { setChecked(!checked); props.onClick(); }}
+      />
+      <span class="checkbox-label">{props.ingredientObj.name}</span>
+    </label>
   );
 }
